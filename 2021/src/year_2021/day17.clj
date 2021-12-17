c5d038b89ac69391d4ca537722618e4461b44f44
Jorge Barrios <jorge@bandpage.com>
2021-12-17 10:13:02 -0800

    Day 17

---
+++ 2021/src/year_2021/day17.clj
@@ -0,0 +1,37 @@
+(ns year_2021.day17)
+(require '[clojure.string :as str])
+
+(defn trajectory [startx starty startxv startyv txmin txmax tymin tymax]
+  (loop [x startx y starty xv startxv yv startyv maxy 0]
+    (cond
+      (< y tymin)  nil
+      (or (and (pos? xv)
+               (> x txmax))
+          (and (neg? xv)
+               (< x txmin))) nil
+      (and (>= x txmin) (<= x txmax)
+           (>= y tymin) (<= y tymax)) {:velocity [startxv startyv] :maxy maxy}
+      :else (let [new-x (+ x xv)
+                  new-y (+ y yv)
+                  new-xv (cond (> xv 0) (dec xv)
+                               (< xv 0) (inc xv)
+                               :else 0)                  
+                  new-yv (dec yv)
+                  new-maxy (if (> new-y maxy) new-y maxy)]
+              (recur new-x new-y new-xv new-yv new-maxy)))))
+
+(defn solve1 []
+  ;; target area: x=175..227, y=-134..-79
+  (let [
+        xrange (range 0 228)
+        yrange (range -134 1000)
+        results (filter some?
+                        (for [xv xrange yv yrange]
+                          (trajectory 0 0 xv yv 175 227 -134 -79)))
+        maxy (apply max (map :maxy results))
+        velocities (set (map :velocity results))]
+    (println "Max height:" maxy)
+    (println "Velocities:" (count velocities))))
+
+(defn main []
+  (solve1))
