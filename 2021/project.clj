ec6597ad6b951f6085424e304195a49526339a07
Jorge Barrios <jorge@bandpage.com>
2021-12-22 22:33:06 -0800

    Day 18

--- 2021/project.clj
+++ 2021/project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :main year_2021.day17/main
+  :main year_2021.day18/main
   :repl-options {:init-ns year-2021.core
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :jvm-opts ["-Xmx1g" "-Xss1g" "-XX:ThreadStackSize=2048"]
