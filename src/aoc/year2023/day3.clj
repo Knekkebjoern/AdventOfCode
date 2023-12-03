bcd26b3a56592ef579725349ba1e9caeee5cfee9
Jorge Barrios <jorge.barrios@gmail.com>
2023-12-03 13:33:04 -0800

    2023 day3 part 2

--- src/aoc/year2023/day3.clj
+++ src/aoc/year2023/day3.clj
@@ -12,7 +12,16 @@
 
 (def nums #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9})
 
-(defn find-all [line]
+(defn get-input []
+  (let [line (slurp input-filename)
+        chars-per-line (str/index-of line "\n")
+        padding  (apply str (concat (take chars-per-line (repeat "."))))
+        line (apply str (concat "." padding "\n" line padding "\n"))
+        line (str/replace line #"\n" "..")
+        chars-per-line (+ chars-per-line 2)]
+    [line chars-per-line]))
+
+(defn find-all-nums [line]
   (loop [line line i 0 res []]
     (if (empty? line)
       res
@@ -34,25 +43,57 @@
                       (subs line leftn (inc rightn)))]
     (not (empty? (str/replace around #"\.|\d" "")))))
 
-(defn solve1 []
-  (let [line (slurp input-filename)
-        chars-per-line (str/index-of line "\n")
-        padding  (apply str (concat (take chars-per-line (repeat "."))))
-        line (apply str (concat "." padding "\n" line padding "\n"))
-        line (str/replace line #"\n" "..")
-        chars-per-line (+ chars-per-line 2)
-        ps (find-all line)
+(defn solve1 [line chars-per-line]
+  (let [ps (find-all-nums line)
         hits (filter #(touches-symbol? % line chars-per-line) ps)]
     (reduce + (map #(Integer/parseInt %) (map second hits)))
     ))
 
-(defn solve2 [input]
+
+(defn find-all-gears [line]
+  (loop [line line i 0 res []]
+    (if (empty? line)
+      res
+      (let [num (apply str (take-while #(= \* %) line))
+            l (count num)]
+        (recur (drop (+ 1 l) line) (+ 1 i l) (if (pos? l)
+                                               (conj res [i num])
+                                               res))))))
+
+(defn get-gear-ratio [pos indices chars-per-line]
+  (let [next-to (fn [x] [(dec x) x (inc x)])
+        is (concat (next-to pos)
+                   (next-to (- pos chars-per-line))
+                   (next-to (+ pos chars-per-line)))
+        nums  (distinct (for [i is
+                              :when (get indices i)]
+                          (get indices i)))]
+    (println nums)
+    (if (=  (count nums) 2)
+      (apply * nums)
+      nil)))
+
+(defn solve2 [line chars-per-line]
+  (let [ps (find-all-nums line)
+        indices (loop [ps ps res {}]
+                  (if (empty? ps)
+                    res
+                    (let [[x s] (first ps)
+                          xs  (apply merge (for [xx (range x  (+ x (count s)))]
+                                             {xx (Integer/parseInt s)}))]
+                      (recur (rest ps) (apply merge res xs)))))
+        gears (find-all-gears line)
+        ratios (for [[pos _] gears]
+                 (get-gear-ratio pos indices chars-per-line))]
+    (apply + (filter #(not (nil? %)) ratios))
+    )
   )
 
 (defn solve []
   (let [[part1 part2] [nil nil]
-        part1 (solve1)
-        part2 (solve2)
+        [line chars-per-line] (get-input)
+        part1 (solve1 line chars-per-line)
+        part2 (solve2 line chars-per-line)
         ]
     {:part1 part1 :part2 part2}))
 
