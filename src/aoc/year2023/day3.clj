c171c9f5f295e774e3adb02b496fb9ff030e986f
Jorge Barrios <jorge.barrios@gmail.com>
2023-12-03 09:25:24 -0800

    2023 day3 part 1

--- src/aoc/year2023/day3.clj
+++ src/aoc/year2023/day3.clj
@@ -1,24 +1,58 @@
-(ns aoc.year2018.day1)
-(require '[clojure.string :as str])
-(require '[clojure.pprint :as pprint])
-(require '[clojure.set :as set])
+(ns aoc.year2023.day3
+  (:require [clojure.string :as str]
+            [clojure.pprint :as pprint]
+            [clojure.set :as cset]))
 
-(def input-filename "inputs/2023/dayX.txt")
+(comment
+  (aoc.core/fetch-input! 2023 3)
+  )
 
-(defn get-input []
-  (let [lines (slurp input-filename)]
-    ()))
+(def input-filename "inputs/2023/day3.txt")
+;;(def input-filename "inputs/2023/day3-test.txt")
 
-(defn solve1 [input]
-  (let []
+(def nums #{\0 \1 \2 \3 \4 \5 \6 \7 \8 \9})
 
+(defn find-all [line]
+  (loop [line line i 0 res []]
+    (if (empty? line)
+      res
+      (let [num (apply str (take-while #(contains? nums %) line))
+            l (count num)]
+        (recur (drop (+ 1 l) line) (+ 1 i l) (if (pos? l)
+                                               (conj res [i num])
+                                               res))))))
+
+(defn touches-symbol? [[ind s] line chars-per-line]
+  (let [left (dec ind)
+        leftp (- left chars-per-line)
+        leftn (+ left chars-per-line)
+        right (+ ind (count s))
+        rightp (- right chars-per-line)
+        rightn (+ right chars-per-line)
+        around (apply str (subs line leftp (inc rightp))
+                      (subs line left (inc right))
+                      (subs line leftn (inc rightn)))]
+    (not (empty? (str/replace around #"\.|\d" "")))))
+
+(defn solve1 []
+  (let [line (slurp input-filename)
+        chars-per-line (str/index-of line "\n")
+        padding  (apply str (concat (take chars-per-line (repeat "."))))
+        line (apply str (concat "." padding "\n" line padding "\n"))
+        line (str/replace line #"\n" "..")
+        chars-per-line (+ chars-per-line 2)
+        ps (find-all line)
+        hits (filter #(touches-symbol? % line chars-per-line) ps)]
+    (reduce + (map #(Integer/parseInt %) (map second hits)))
     ))
+
+(defn solve2 [input]
+  )
 
 (defn solve []
   (let [[part1 part2] [nil nil]
-        input (get-input)
-        part1 (solve1 input)
-        ;part2 (solve2 input max-x max-y)
+        part1 (solve1)
+        part2 (solve2)
         ]
     {:part1 part1 :part2 part2}))
 
