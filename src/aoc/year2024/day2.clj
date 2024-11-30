f33b2a14e77f4962962c56824a5515d4f88aa42f
Jorge Barrios <jorge.barrios@gmail.com>
2024-11-30 11:00:14 -0800

    Generated this year's files.

---
+++ src/aoc/year2024/day2.clj
@@ -0,0 +1,30 @@
+(ns aoc.year2024.day2
+  (:require [clojure.string :as str]
+            [clojure.pprint :as pprint]
+            [clojure.set :as cset]))
+
+(comment
+  (aoc.core/fetch-input! 2024 2)
+  )
+
+(def input-filename "inputs/2024/day2.txt")
+
+(defn get-input []
+  (let [lines (slurp input-filename)]
+    ))
+
+(defn solve1 [input]
+  )
+
+(defn solve2 [input]
+  )
+
+(defn solve []
+  (let [[part1 part2] [nil nil]
+        input (get-input)
+        part1 (solve1 input)
+        part2 (solve2 input)
+        ]
+    {:part1 part1 :part2 part2}))
+
+(defn status [] "*")
