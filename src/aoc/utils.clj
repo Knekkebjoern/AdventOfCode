d86844b3496471f5abd4e9b1e6c34ee5ab199689
Jorge Barrios <jorge.barrios@gmail.com>
2024-12-02 21:30:26 -0800

    2024 day 3

--- src/aoc/utils.clj
+++ src/aoc/utils.clj
@@ -1,20 +1,15 @@
 (ns aoc.utils)
 
 (defn drop-indices
-  "Returns coll without the elements indexed by `is`.
+  "Returns coll without the elements indexed by `indices`.
 (drop-indices [1 2 3 4 5 6] [0 1 4]) ;; => [3 4 6]
 "
-  [coll is]
+  [coll indices]
   {:pre [(sequential? coll)
-         (sequential? is)
-         (every? int? is)]}
-  (let [is (set is)]
-    (loop [coll coll
-           i 0
-           res []]
-      (if (empty? coll)
-        res
-        (let [res (if (is i)
-                    res
-                    (conj res (first coll)))]
-          (recur (rest coll) (inc i) res))))))
+         (sequential? indices)
+         (every? int? indices)]}
+  (let [indices (set indices)]
+    (keep-indexed (fn [i n]
+                    (if (indices i) nil n) ; set type lookup returns nil or `i`
+                    )
+                  coll)))
