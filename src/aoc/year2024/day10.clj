a6b25f6348af17c89cbe99c3938eaf3c9a523d27
Jorge Barrios <jorge.barrios@gmail.com>
2024-12-14 20:42:43 -0800

    2024 day 10

--- src/aoc/year2024/day10.clj
+++ src/aoc/year2024/day10.clj
@@ -1,30 +1,64 @@
 (ns aoc.year2024.day10
   (:require [clojure.string :as str]
             [clojure.pprint :as pprint]
-            [clojure.set :as cset]))
+            [clojure.set :as cset]
+            [aoc.io :as io]))
 
 (comment
-  (aoc.core/fetch-input! 2024 10)
-  )
+  (aoc.core/fetch-input! 2024 10))
 
 (def input-filename "inputs/2024/day10.txt")
+;;(def input-filename "inputs/2024/day10-test.txt")
 
 (defn get-input []
-  (let [lines (slurp input-filename)]
-    ))
+  (update-vals
+   (io/parse-lines-as-grid (io/split-lines (io/read-file input-filename)))
+   (fn [v] (parse-long (str v)))))
+
+(defn get-surrounding [lookup [px py]]
+  (into {} (for [[x y] [[1 0] [-1 0] [0 1] [0 -1]]
+                 :let [nx (+ px x)
+                       ny (+ py y)]
+                 :when (and (not (= 0 x y))
+                            (contains? lookup [nx ny]))]
+             {[nx ny] (get lookup [nx ny])})))
+
+(defn next-steps-for-pos [lookup pos]
+  (let [height (get lookup pos)
+        surr (get-surrounding lookup pos)]
+    (for [[k v] surr
+          :when (= v (inc height))]
+      k)))
+
+(defn get-all-trails [lookup]
+  (loop [trails (map vector (keys (filter (fn [[_ v]] (= v 0)) lookup)))
+         res []]
+    (if (empty? trails)
+      res
+      (let [trail (first trails)
+            last-pos (last trail)]
+        (if (= 9 (get lookup last-pos))
+          (recur (rest trails) (conj res trail))
+          (let [next-steps (next-steps-for-pos lookup last-pos)
+                new-trails (map (fn [step] (conj trail step)) next-steps)
+                trails (concat (rest trails) new-trails)]
+            (recur trails res)))))))
 
 (defn solve1 [input]
-  )
+  (let [trails (get-all-trails input)]
+    (count (set (for [trail trails]
+                  [(first trail) (last trail)])))))
 
 (defn solve2 [input]
-  )
+  (count  (get-all-trails input)))
 
 (defn solve []
   (let [[part1 part2] [nil nil]
         input (get-input)
         part1 (solve1 input)
-        part2 (solve2 input)
-        ]
+        part2 (solve2 input)]
     {:part1 part1 :part2 part2}))
+
+(println (solve))
 
 (defn status [] "*")
